ca4f6ec1555dcd86a5b68f12a47ab888e07ffcae
hIt Vir <hienhao021198@gmail.com>
2023-05-10 15:28:31 +0700

    comment api url

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'http://localhost:3000/api';
-export const BASE_URL = 'https://3387-222-252-28-142.ngrok-free.app/api';
+export const BASE_URL = 'http://localhost:3000/api';
+// export const BASE_URL = 'https://3387-222-252-28-142.ngrok-free.app/api';
